cf7c2188ca32a445a72a3849e092d2e7d2b3e8bf
Benjamin Vollmers <benjamin.vollmers@gmail.com>
2022-12-10 14:11:01 +0100

    Added delete button

--- DoroPWA/src/app/marketplace/marketcard/marketcard.component.ts
+++ DoroPWA/src/app/marketplace/marketcard/marketcard.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MarketApiService } from 'src/app/services/market-api.service';
 
 @Component({
   selector: 'app-marketcard',
@@ -8,9 +9,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MarketcardComponent implements OnInit {
   @Input() marketItem: any;
 
-  constructor() { }
+  constructor(private marketApi: MarketApiService) { }
 
   ngOnInit(): void {
+  }
+
+  onSelled() {
+    this.marketApi.deleteMarketItem({"id": this.marketItem.id}).subscribe(
+      (response) => {console.log("Hello World")}
+    );
+
   }
 
 }
